47a30e163d32a2494952287dfcdc388240c421e7
JUCINEI LUIZ BELO <jucinei.belo@alunos.sc.senac.br>
2025-10-22 20:25:43 -0300

    arquitetura do projeto

---
+++ stayfine/src/main/java/com/stayfine/stayfine/StayfineApplication.java
@@ -0,0 +1,13 @@
+package com.stayfine.stayfine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayfineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayfineApplication.class, args);
+	}
+
+}
---
+++ stayfine/src/test/java/com/stayfine/stayfine/StayfineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayfine.stayfine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayfineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
